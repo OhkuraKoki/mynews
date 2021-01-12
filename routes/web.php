8e3cf0db05baac458cae2060e113c639f5cab215
OhkuraKoki <k-ohkura@cmind-co.jp>
2021-01-12 19:33:18 +0900

    viwとコントローラー

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'App\Http\Controllers\Admin\ProfileController@add');
     Route::get('profile/edit', 'App\Http\Controllers\Admin\ProfileController@add');
 });
+
